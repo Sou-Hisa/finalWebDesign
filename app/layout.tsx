23f444a6206bc86893fbfa5544f72f2b57a4a034
fengyenchia <yanjia20050504@gmail.com>
2026-06-09 11:03:22 +0800

    feat:update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Ma_Shan_Zheng, Noto_Serif_TC, Noto_Sans_TC } from "next/font/google";
 import "./globals.css";
 import BgMusic from "../component/BgMusic";
+import { url } from "inspector/promises";
 
 const maShangZheng = Ma_Shan_Zheng({
   weight: "400",
@@ -35,7 +36,14 @@ export const metadata: Metadata = {
     description: "一個黑暗童話密室逃脫遊戲，在充滿詭異氛圍的糖果屋中尋找線索、解開謎題，逃離女巫的掌控。",
     url: "https://final-web-design-dusky.vercel.app/",
     siteName: "逃離糖果屋",
-    images: "images/ch01_bg.png",
+    images: [
+      {
+        url: "images/ch01_bg.png",
+        width: 1200,
+        height: 630,
+        alt: "逃離糖果屋",
+      }
+    ]
   },
 };
 
